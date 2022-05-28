bf53ab5431f729785a605f3ae9bb07b522fe3ad8
Jackson Karel <jackson.karel2@gmail.com>
2022-05-28 07:36:50 -0700

    Refactoring

--- gen_data.py
+++ gen_data.py
@@ -4,8 +4,6 @@ from rasa.shared.nlu.training_data.formats.rasa_yaml import RasaYAMLWriter
 from rasa.shared.nlu.training_data.training_data import TrainingData
 from rasa.model_training import train_nlu
 
-from data import NLP, ONTOLOGY
-
 def gen_data():
     lookup_data = TrainingData()
     ent_syn_data = TrainingData()
@@ -14,29 +12,24 @@ def gen_data():
     with open(skills_path) as json_file:
         skills = json.load(json_file)
     
-    new_json = {}
     ent_syn = {}
     skill_lookup = {'name': 'skill', 'elements': []}
-    roles = [NLP, ONTOLOGY]
-    for role in roles:
-        low_skills = {}
-        for skill in skills[role].items():
-            s_value = skill[1]
-            if skill[0].islower():
-                s_name = skill[0]
-            else:
-                s_value["capitalized"] = skill[0]
-                s_name = skill[0].lower()
+    low_skills = {}
+    for skill in skills.items():
+        s_value = skill[1]
+        if skill[0].islower():
+            s_name = skill[0]
+        else:
+            s_value["capitalized"] = skill[0]
+            s_name = skill[0].lower()
 
-            low_skills[s_name] = s_value
-
-        new_json[role] = low_skills
+        low_skills[s_name] = s_value
     
-        for skill in low_skills:
-            if "synonym" in low_skills[skill]:
-                ent_syn[skill] = low_skills[skill]["synonym"]
-            
-            skill_lookup["elements"].append(skill)
+    for skill in low_skills:
+        if "synonym" in low_skills[skill]:
+            ent_syn[skill] = low_skills[skill]["synonym"]
+        
+        skill_lookup["elements"].append(skill)
     
     ent_syn_data.entity_synonyms = ent_syn
     lookup_data.lookup_tables = [skill_lookup]
@@ -45,6 +38,6 @@ def gen_data():
     yaml_writer.dump("nlu/data/skill_lookup.yml", lookup_data)
 
     with open(skills_path, 'w') as outfile:
-        json.dump(new_json, outfile)
+        json.dump(low_skills, outfile)
 
     train_nlu("nlu/config.yml", "nlu/data", "models", "resume")
